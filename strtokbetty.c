f1da5cb096d4991a0e4bf704f73ec01945ef537d
Alpho8 <alphonseoketch8@gmail.com>
2025-05-13 10:59:34 +0300

    tokenize

---
+++ strtokbetty.c
@@ -0,0 +1,47 @@
+#include "shell.h"
+
+/**
+ * tokenize - Splits a line into tokens (words).
+ * @line: The input line to tokenize.
+ *
+ * Description: Uses strtok to split the input line based on whitespace
+ * characters (space, tab, carriage return, newline). Allocates and
+ * resizes memory as needed to hold all tokens.
+ *
+ * Return: A null-terminated array of strings (tokens).
+ */
+char **tokenize(char *line)
+{
+	int bufsize = 64, i = 0;
+	char **tokens = malloc(sizeof(char *) * bufsize);
+	char *token;
+
+	if (!tokens)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, " \t\r\n");
+	while (token != NULL)
+	{
+		tokens[i++] = token;
+
+		if (i >= bufsize)
+		{
+			bufsize += 64;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				perror("realloc");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		token = strtok(NULL, " \t\r\n");
+	}
+
+	tokens[i] = NULL;
+	return (tokens);
+}
+
