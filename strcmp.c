cad45ac962d3a67c509498ea369a97e9779dbe09
Alpho8 <alphonseoketch8@gmail.com>
2025-05-14 04:09:18 +0300

    compare strings

---
+++ strcmp.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string to compare.
+ * @s2: The second string to compare.
+ *
+ * Return: An integer less than, equal to, or greater than 0, if s1 is found,
+ * respectively, to be less than, to match, or be greater than s2.
+ */
+int _strcmp(const char *s1, const char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
